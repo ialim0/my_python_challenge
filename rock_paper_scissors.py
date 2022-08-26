44af906784ca30cb192610511891f9b5584c265c
ialim0 <i.alim0229@gmail.com>
2022-08-26 06:59:20 +0000

    Best rock paper scissors game

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -28,15 +28,29 @@ scissors = '''
 
 #Write your code below this line 👇
 tab=[rock,paper,scissors]
+try:
+    your_choice=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+    computer_choice=random.randint(0,2)
 
-your_choice=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
-print(f"You choose:\n{tab[your_choice]}")
-computer_choice=random.randint(0,2)
 
-print(f'Computer choose:\n{tab[computer_choice]}')
-if (your_choice==0 and computer_choice==2) or (your_choice==2 and computer_choice==1) or (your_choice==1 and computer_choice==0):
-    print("You win")
-elif your_choice==computer_choice :
-    print("You have to replay")
-else :
-    print("You lose.")
+    if (your_choice==0 and computer_choice==2) or (your_choice==2 and computer_choice==1) or (your_choice==1 and computer_choice==0):
+        print(f"You choose:\n{tab[your_choice]}")
+        print(f'Computer choose:\n{tab[computer_choice]}')
+        print("You win")
+    elif your_choice==computer_choice :
+        print(f"You choose:\n{tab[your_choice]}")
+        print(f'Computer choose:\n{tab[computer_choice]}')
+        print("You have to replay")
+
+    else :
+        print("You lose.")
+except ValueError:
+    print('You lose')
+
+    
+
+
+
+    
+    
+    
