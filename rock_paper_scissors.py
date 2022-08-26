be396d1f291b20d047b7120a6ea1e35657a2c461
ialim0 <i.alim0229@gmail.com>
2022-08-26 05:44:38 +0000

    rock_paper scissors

---
+++ rock_paper_scissors.py
@@ -0,0 +1,42 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+tab=[rock,paper,scissors]
+
+your_choice=int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(f"You choose:\n{tab[your_choice]}")
+computer_choice=random.randint(0,2)
+
+print(f'Computer choose:\n{tab[computer_choice]}')
+if (your_choice==0 and computer_choice==2) or (your_choice==2 and computer_choice==1) or (your_choice==1 and computer_choice==0):
+    print("You win")
+elif your_choice==computer_choice :
+    print("You have to replay")
+else :
+    print("You lose.")
